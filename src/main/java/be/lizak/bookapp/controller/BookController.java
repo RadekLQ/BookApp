1e57b5d571b7e3eaf89f713286da2b96e42278e4
RadekLQ <radoslaw.lizak@gmail.com>
2017-11-16 11:38:43 +0100

    1.6.3

--- src/main/java/be/lizak/bookapp/controller/BookController.java
+++ src/main/java/be/lizak/bookapp/controller/BookController.java
@@ -4,10 +4,7 @@ import be.lizak.bookapp.model.Book;
 import be.lizak.bookapp.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/books")
@@ -32,7 +29,21 @@ public class BookController {
 
     @RequestMapping(method = RequestMethod.DELETE)
     public ResponseEntity deleteBook(@RequestBody Book book) {
-        return
+        return ResponseEntity.ok(service.addBook(book));
     }
 
+    @RequestMapping(value = "/author/{author}",method = RequestMethod.GET)
+    public ResponseEntity findByAuthor(@PathVariable String author) {
+        return ResponseEntity.ok(service.findByAuthor(author));
+    }
+
+    @RequestMapping(value = "/isbn/{isbn}", method = RequestMethod.GET)
+    public ResponseEntity findByIsbn(@PathVariable String isbn) {
+        return ResponseEntity.ok(service.findByISBN(isbn));
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public ResponseEntity updateBook(@RequestBody Book book) {
+        return ResponseEntity.ok(service.updateBook(book));
+    }
 }
